7a8b183da69095ca0224dcd66d525eb5206eead2
Gabriel Souza <gabrielsaojoao12@gmail.com>
2022-05-07 21:25:32 -0300

    Integrated frontend with backend

--- server/src/use-cases/submit-feedback-use-case.ts
+++ server/src/use-cases/submit-feedback-use-case.ts
@@ -26,10 +26,6 @@ export class SubmitFeedbackUseCase{
       
     }
 
-    if(screenshot && !screenshot.startsWith('data-image/png;base64')){
-      throw new Error("Invalid screenshot format.");
-    }
-
     await this.feedbacksRepository.create({
       type,
       comment,
@@ -42,6 +38,7 @@ export class SubmitFeedbackUseCase{
         `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
         `<p>Tipo do feedback: ${type}</p>`,
         `<p>Comentário: ${comment}</p>`,
+        screenshot ? `<img src="${screenshot}"` : null,
         `</div>`
       ].join('\n'),
     })
